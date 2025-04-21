4a1f2f6ec9422855d870b5aea71044a5e0c7f19f
satamkundu <satamkundu67@gmail.com>
2025-04-21 12:26:52 +0530

    creating migration files

---
+++ database/migrations/2025_04_21_065020_families.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('families', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->date('birthdate');
+            $table->string('mobile')->unique();
+            $table->text('address');
+            $table->string('state');
+            $table->string('city');
+            $table->string('pincode');
+            $table->enum('marital_status', ['Married', 'Unmarried']);
+            $table->date('wedding_date')->nullable();
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('families');
+    }
+};
---
+++ database/migrations/2025_04_21_065027_family_members.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('family_members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('family_id')->constrained('families')->onDelete('cascade');
+            $table->string('name');
+            $table->date('birthdate');
+            $table->enum('marital_status', ['Married', 'Unmarried']);
+            $table->date('wedding_date')->nullable();
+            $table->string('education')->nullable();
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('family_members');
+    }
+};
